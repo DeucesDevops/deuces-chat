bfee5c90361779822c8bdf7c26042c096b2cebc8
DeucesDevops <132962088+DeucesDevops@users.noreply.github.com>
2023-12-28 19:36:21 +0000

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis'
 
 export const db = new Redis({
-  url: 'https://eu1-fluent-lacewing-39112.upstash.io',
-  token: 'AZjIASQgYjdmYjE2NDAtODlhYi00NGQ2LWE1ODYtYzE2YzJlMTcyODY0ZDU4NDBlZTliNmY1NGE5MTk5ZWI4NGE3MWQyYWM2MTc=',
+  url:process.env.UPSTASH_REDIS_REST_URL,
+  token:UPSTASH_REDIS_REST_TOKEN,
 })
